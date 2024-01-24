f1bd6b756810580aa720c4e6dc010ac21aa7830f
Mark Makarenko <speakland@bk.ru>
2024-01-24 15:32:28 +0300

    changes 1

--- src/FileExample.java
+++ src/FileExample.java
@@ -2,30 +2,32 @@ import java.io.*;
 import java.util.Objects;
 import java.util.Scanner;
 
+/*
+  The FileExample class provides functionality for working with text files in Java.
+ */
 public class FileExample {
     private File file;
 
+    /*
+      Constructor that takes a file name and creates a FileExample object.
+      @param fileName The name of the file.
+     */
     public FileExample(String fileName) {
         this.file = new File(fileName);
     }
 
-    public FileExample(File file) {
-        this.file = file;
-    }
-
-    public File getFile() {
-        return file;
-    }
-
-    public void setFile(File file) {
-        this.file = file;
-    }
-
+    /*
+      Method for inserting text into the file from the console.
+     @throws IOException Exception that may occur during file operations.
+     */
     public void fileInsert() throws IOException {
         try (FileWriter writer = new FileWriter(file, true)) {
             Scanner scanner = new Scanner(System.in);
+
+            // Read input from the console until "exit" is entered.
             String line = scanner.nextLine();
             while (!Objects.equals(line, "exit")) {
+                // Write the input line to the file and append a new line character.
                 writer.write(line);
                 writer.write("\n");
                 line = scanner.nextLine();
@@ -33,25 +35,43 @@ public class FileExample {
         }
     }
 
+    /*
+     * Method for extracting and printing the content of the file.
+     * @throws IOException Exception that may occur during file operations.
+     */
     public void fileExtraction() throws IOException {
         try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
             String line;
+
+            // Read each line from the file and print it to the console.
             while ((line = reader.readLine()) != null) {
                 System.out.println(line);
             }
         }
     }
 
+    /*
+      Method to erase the content of the file.
+      @throws IOException Exception that may occur during file operations.
+     */
     public void eraseFile() throws IOException {
         try (FileWriter writer = new FileWriter(file, false)) {
+            // Write an empty string to the file, effectively erasing its content.
             writer.write("");
-
         }
     }
+
+    /*
+      Method to copy content from another file to the current file.
+      @param sourceFile The file from which to copy the content.
+      @throws IOException Exception that may occur during file operations.
+     */
     public void copyFromFile(File sourceFile) throws IOException {
         try (BufferedReader reader = new BufferedReader(new FileReader(sourceFile));
              BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
             String line;
+
+            // Read each line from the source file and write it to the current file.
             while ((line = reader.readLine()) != null) {
                 writer.write(line);
                 writer.newLine();
@@ -59,5 +79,3 @@ public class FileExample {
         }
     }
 }
-
-
--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,40 @@
 import java.io.File;
 import java.io.IOException;
 
-/**
- * The Main class contains the main method to demonstrate the functionality of the FileExample class.
+/*
+ The Main class contains the main method to demonstrate the functionality of the FileExample class.
  */
 public class Main {
     public static void main(String[] args) throws IOException {
         // Specify the name of the test file.
-        String fileName = "testFile";
-
-        // Specify the name of the source file for copying.
-        File sourceFile = new File("sourceFile");
-
+        String fileName = "testFile.txt";
         // Create a FileExample object with the specified test file name.
         FileExample testFile = new FileExample(fileName);
+
+        String sourceName = "sourceFile.txt";
+        //creating File object
+        File sourceFile = new File(sourceName);
+
+        //creating file, if it doesn't exist
+        if (sourceFile.createNewFile()) {
+            System.out.println("File created: " + sourceFile.getAbsolutePath());
+        } else {
+            System.out.println("File already exists: " + sourceFile.getAbsolutePath());
+        }
 
         // Call the testFunc method to demonstrate file operations.
         testFunc(testFile, sourceFile);
     }
 
-    /**
-     * Method to test various file operations using the FileExample class.
-     *
-     * @param testFile   The FileExample object to perform operations on.
-     * @param sourceFile The source file for copying content.
-     * @throws IOException Exception that may occur during file operations.
+    /*
+     Method to test various file operations using the FileExample class.
+      @param testFile   The FileExample object to perform operations on.
+      @param sourceFile The source file for copying content.
+      @throws IOException Exception that may occur during file operations.
      */
     public static void testFunc(FileExample testFile, File sourceFile) throws IOException {
         // Insert text into the file from the console.
-        System.out.println("Insert into file from console (type 'exit' to stop):");
+        System.out.println("Insert into file from console (type 'exit' to continue):");
         testFile.fileInsert();
 
         // Display the content of the file.
